a3cfc7352f50d54ab0e6686348a4fe8cdde77bcc
Your Name <you@example.com>
2019-09-22 00:42:19 +0300

    getSeason file

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import getSeason from './getSeason'
 
-const getSeason = (lat, month) => {
-    if (month>2 && month<9) {
-        return lat>0 ? 'summer' : 'winter'
-    } else {
-        return lat>0 ? 'winter' : 'summer'
-    }
-}
+
+// const getSeason = (lat, month) => {
+//     if (month>2 && month<9) {
+//         return lat>0 ? 'summer' : 'winter'
+//     } else {
+//         return lat>0 ? 'winter' : 'summer'
+//     }
+// }
+
+var month = new Date().getMonth()
+
 
 const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth())
+    const season = getSeason(props.lat, month)
     return (
         <div>{season}</div>
     )
