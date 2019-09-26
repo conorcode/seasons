c3064d3082c49354cc9a55717ef304e148353359
conorcode <conorcode@conorcode.com>
2019-09-26 23:04:53 +0300

    test ssh

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner'
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
-
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
 
-  componentDidUpdate() {
-
-    
-  }
+  componentDidUpdate() {}
 
   render() {
     if (this.state.errorMessage) {
@@ -25,7 +21,7 @@ class App extends React.Component {
     }
 
     if (this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
     }
 
     return <Spinner />;
